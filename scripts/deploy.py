ea0c375705976dbf622fcdd1162cd0bdd1772c3d
Angel Catanzariti <angelcatanzariti1@gmail.com>
2022-02-14 15:51:46 -0300

    .env.example + deploy optimization

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,7 +3,7 @@ from scripts.helpful_scripts import get_account
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMyProject.deploy({"from": account})
+    fund_me = FundMyProject.deploy({"from": account}, publish_source=True)
     print(f"Contract deployed to {fund_me.address}")
 
 def main():
