dffcf0afcf8da81878d24d3ca81c00292c9719b2
Angel Catanzariti <angelcatanzariti1@gmail.com>
2022-02-14 16:17:55 -0300

    network-agnostic price feed

--- contracts/FundMyProject.sol
+++ contracts/FundMyProject.sol
@@ -10,8 +10,10 @@ contract FundMyProject {
     address owner;
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
+    AggregatorV3Interface public priceFeed;
 
-    constructor() public{
+    constructor(address _priceFeed) public{
+        priceFeed = AggregatorV3Interface(_priceFeed);
         owner = msg.sender;
     }
 
@@ -24,13 +26,11 @@ contract FundMyProject {
 
     //Get Aggregator Interface version
     function getVersion() public view returns(uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e); //Rinkeby
-        priceFeed.version();
+        return priceFeed.version();
     }
 
     //ETH -> USD conversion rate
     function getPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         (,int256 answer,,,) = priceFeed.latestRoundData();
         return uint256(answer * 10000000000);
     }
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,34 @@
-from brownie import FundMyProject
+from brownie import FundMyProject, network, config, MockV3Aggregator
 from scripts.helpful_scripts import get_account
+
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMyProject.deploy({"from": account}, publish_source=True)
+    # Pass the price feed address to our fundmyproject contract
+
+    # If we are on a persistent network like Rinkeby, use the associated address
+    # Otherwise, deploy mocks
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        print(f"Active network is {network.show_active()}")
+        print("Deploying Mocks...")
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 2000000000000000000, {"from": account}
+        )
+        price_feed_address = mock_aggregator.address
+        print("Mocks Deployed!")
+
+    print("Deploying FundMyProject...")
+    fund_me = FundMyProject.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
     print(f"Contract deployed to {fund_me.address}")
+
 
 def main():
     deploy_fund_me()
