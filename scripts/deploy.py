2d2dabeea5e5db866dd9f279689543cb337db469
Angel Catanzariti <angelcatanzariti1@gmail.com>
2022-02-14 16:47:33 -0300

    ganache local network ready

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,5 @@
 from brownie import FundMyProject, network, config, MockV3Aggregator
-from scripts.helpful_scripts import deploy_mocks, get_account, deploy_mocks
+from scripts.helpful_scripts import deploy_mocks, get_account, deploy_mocks, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from web3 import Web3
 
 def deploy_fund_me():
@@ -7,7 +7,7 @@ def deploy_fund_me():
     # Pass the price feed address to our fundmyproject contract
     # If we are on a persistent network like Rinkeby, use the associated address
     # Otherwise, deploy mocks
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
