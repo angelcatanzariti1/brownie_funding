1573b244bc4e039f860d888d1e1740b8649f39d5
Angel Catanzariti <angelcatanzariti1@gmail.com>
2022-02-14 14:37:17 -0300

    dependencies settings

--- contracts/FundMyProject.sol
+++ contracts/FundMyProject.sol
@@ -1,8 +1,8 @@
-//SPDX-Licence-Identifier: MIT
-pragma solidity >=0.6.6 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@chainlink/contracts/src/v0.8/vendor/SafeMathChainlink.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 contract FundMyProject {
     using SafeMathChainlink for uint256;
@@ -29,7 +29,7 @@ contract FundMyProject {
     }
 
     //ETH -> USD conversion rate
-    function getPrice() public view returnt(uint256){
+    function getPrice() public view returns(uint256){
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         (,int256 answer,,,) = priceFeed.latestRoundData();
         return uint256(answer * 10000000000);
@@ -56,6 +56,6 @@ contract FundMyProject {
             addressToAmountFunded[funder] = 0;
         }
         //reset funders array
-        funders = new address[];
+        funders = new address[](0);
     }
 }
