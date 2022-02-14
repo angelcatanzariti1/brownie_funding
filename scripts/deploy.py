3e63974cf11139f90c5d6dd70ba64f303be786f9
Angel Catanzariti <angelcatanzariti1@gmail.com>
2022-02-14 16:31:35 -0300

    refactor

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,11 +1,10 @@
 from brownie import FundMyProject, network, config, MockV3Aggregator
-from scripts.helpful_scripts import get_account
-
+from scripts.helpful_scripts import deploy_mocks, get_account, deploy_mocks
+from web3 import Web3
 
 def deploy_fund_me():
     account = get_account()
     # Pass the price feed address to our fundmyproject contract
-
     # If we are on a persistent network like Rinkeby, use the associated address
     # Otherwise, deploy mocks
     if network.show_active() != "development":
@@ -13,13 +12,9 @@ def deploy_fund_me():
             "eth_usd_price_feed"
         ]
     else:
-        print(f"Active network is {network.show_active()}")
-        print("Deploying Mocks...")
-        mock_aggregator = MockV3Aggregator.deploy(
-            18, 2000000000000000000, {"from": account}
-        )
-        price_feed_address = mock_aggregator.address
-        print("Mocks Deployed!")
+        deploy_mocks()
+        # Use the most recently deployed MockV3Aggregator
+        price_feed_address = MockV3Aggregator[-1].address
 
     print("Deploying FundMyProject...")
     fund_me = FundMyProject.deploy(
