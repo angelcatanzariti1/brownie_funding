925c405a8374373906f9082e33686a324d15c947
Angel Catanzariti <angelcatanzariti1@gmail.com>
2022-02-17 15:17:55 -0300

    extended testing (only owner can withdraw)

--- tests/test_fund_my_project.py
+++ tests/test_fund_my_project.py
@@ -1,5 +1,7 @@
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from scripts.deploy import deploy_fund_me
+from brownie import network, accounts, exceptions
+import pytest
 
 def test_can_fund_and_withdraw():
     account = get_account()
@@ -11,3 +13,11 @@ def test_can_fund_and_withdraw():
     tx2 = fund_me.withdraw({"from": account})
     tx2.wait(1)
     assert fund_me.addressToAmountFunded(account.address) == 0
+
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from": bad_actor})
